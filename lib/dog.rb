602d066c8421e3de924823cf03eed407d5e31129
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-03-08 18:51:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,7 @@
 class Dog 
-  def initialize(dog_name)
-    @this_dogs_name= dog_name
-  end
+ 
   def name= (dog_name) 
-    @this_dogs_name 
+    @this_dogs_name = dog_name
   end
   def name 
     @this_dogs_name
